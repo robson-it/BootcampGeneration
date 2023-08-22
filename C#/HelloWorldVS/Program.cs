220b9e3a6262ffa4cdb3985391b18e924fdd5f6c
Robson A. Rocha <robson_it@outlook.com>
2023-08-22 11:09:12 -0300

    Aula 01 - HelloWorld

---
+++ C#/HelloWorldVS/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorldVS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("A turma Dot Net 03 é topppp!!!");
+        }
+    }
+}
