a241663d32195e2831c184335dfc616420bea1db
Robson A. Rocha <robson_it@outlook.com>
2023-09-21 20:39:14 -0300

    HelloWorld com AspNetCore API + Exercicio

---
+++ AspNetCore/Exercicio01/Controllers/Exercicio01Controller.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exercicio01.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class Exercicio01Controller
+    {
+
+        [HttpGet]
+        public string HelloWorld()
+        {
+            string helloWorld = """
+                **********************************************************************
+                -----------------------------Hello World------------------------------
+                **********************************************************************
+                """;
+            return helloWorld;
+        }
+
+        [HttpGet("/bsm")]
+        public string Bsms()
+        {
+
+            string bsms = """
+                **********************************************************************
+                --------------------------------Bsm's---------------------------------
+                -Orientação ao detalhe
+                -Orientação ao futuro
+                -Mentalidade de crescimento
+                -Comunicação
+                -Persistência
+                -Responsabilidade pessoal
+                -Proatividade
+                -Trabalho em equipe
+                ----------------------------------------------------------------------
+                **********************************************************************
+                """;
+            return bsms;
+        }
+
+        [HttpGet("/objetivos")]
+        public string Objetivos()
+        {
+
+            string objetivos = """
+                **********************************************************************
+                ------------------------------Objetivos------------------------------
+                -Aprender sobre Sql
+                -Aprender sobre Asp Net com o modelo MVC
+                -Desenvolver Soft Skill durante os trabalhos em grupo
+                ---------------------------------------------------------------------
+                **********************************************************************
+                """;
+            return objetivos;
+        }
+
+    }
+}
---
+++ AspNetCore/HelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello Turma Dot Net 03!";
+        }
+
+        [HttpGet("/meunome")]
+        public string MeuNome()
+        {
+            return "Robson Alves Rocha!";
+        }
+    }
+}
