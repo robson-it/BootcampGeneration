545c2d233922e2f687216a0cfd344667825dae3b
Robson A. Rocha <robson_it@outlook.com>
2023-10-19 23:51:13 -0300

    Aula02 - Javascript

--- FrontEnd/Aula_02/assets/script/script.js
+++ FrontEnd/Aula_02/assets/script/script.js
@@ -1,13 +1,81 @@
 let nome = document.getElementById('nome')
 let email = document.getElementById('email')    
 let assunto = document.querySelector('#assunto')//.assunto class
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
 
 function verificarCampos (){
 
-    if(nome.value == "" || email.value == "" || assunto.value == ""){
-        alert('O nome deve ser preenchido!')
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Formulário enviado com sucesso!')
+       
     }else{
-        alert('Dados enviados com sucesso!')
+        alert('Preencha o formulário corretamente antes de enviar!')
+        
     }
 }
+
+function validaNome(){
+    let txt = document.querySelector('#txtNome')
+    if(nome.value.length < 3){
+        txt.style.color = 'red'
+        txt.innerHTML = 'Nome Inválido!'
+        nomeOk = false
+        
+    }else{
+        txt.style.color = 'green'
+        txt.innerHTML = 'Nome Válido!'
+        nomeOk = true
+        
+    }
+}
+
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.style.color = 'red'
+        txtEmail.innerHTML = 'E-mail Inválido'
+        emailOk = false
+        
+    }else{
+        txtEmail.style.color = 'green'
+        txtEmail.innerHTML = 'E-mail Válido!'
+        emailOk = true
+        
+    }
+}
+
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if(assunto.value.length > 100  ){
+        txtAssunto.style.color = 'red'
+        txtAssunto.innerHTML = 'Você ultrapassou o limite de 100 caracteres. Texto Inválido!'
+        txtAssunto.style.display  = 'block'
+        assuntoOk = false
+        
+    }else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+        
+    }
+
+}
+
+
+function mapaZoom(){
+    mapa.style.width = '550px'
+    mapa.style.heght = '400px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '450px'
+    mapa.style.heght = '360px'
+}
+
 
